40aa4ff6d6d1a46714462fb9e78d7c8e00c5e619
AnjanaUdayan <contactme.anjana@gmail.com>
2026-04-23 22:47:50 +0500

    Add files via upload

---
+++ Models/InsuranceEntities.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CarInsurance.Models
+{
+    public class InsuranceEntities : DbContext
+    {
+        public InsuranceEntities() : base("InsuranceEntities")
+        {
+        }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
---
+++ Models/Insuree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarInsurance.Models
+{
+    public class Insuree
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Car Year")]
+        public int CarYear { get; set; }
+
+        [Required]
+        [Display(Name = "Car Make")]
+        public string CarMake { get; set; }
+
+        [Required]
+        [Display(Name = "Car Model")]
+        public string CarModel { get; set; }
+
+        [Required]
+        public bool DUI { get; set; }
+
+        [Required]
+        [Display(Name = "Speeding Tickets")]
+        public int SpeedingTickets { get; set; }
+
+        [Required]
+        [Display(Name = "Full Coverage")]
+        public bool CoverageType { get; set; }
+
+        [Required]
+        public decimal Quote { get; set; }
+    }
+}
